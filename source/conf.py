c503a165963a3b482f3b6ee35a8832802fba76b6
Jae Woo Park <triplepjw@gmail.com>
2017-04-24 11:41:40 -0500

    author list was ugly

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BAGEL'
-copyright = u'2017, Toru Shiozaki and Bess Vlasavljevich and Peter Cherry and Jae Woo Park and Ryan D. Reynolds and Hai-Anh Le and Jheng-Wei Li and Raymond Yiqun Wang and Yeonjun Jeong'
+copyright = u'2017, Shiozaki Group'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -197,7 +197,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'BAGEL.tex', u'BAGEL Documentation',
-   u'Toru Shiozaki and Bess Vlasavljevich and Peter Cherry and Jae Woo Park and Ryan D. Reynolds and Hai-Anh Le and Jheng-Wei Li and Raymond Yiqun Wang and Yeonjun Jeong', 'manual'),
+   u'Shiozaki Group', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,7 +227,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'bagel', u'BAGEL Documentation',
-     [u'Toru Shiozaki and Bess Vlasavljevich and Peter Cherry and Jae Woo Park and Ryan D. Reynolds and Hai-Anh Le and Jheng-Wei Li and Raymond Yiqun Wang and Yeonjun Jeong'], 1)
+     [u'Shiozaki Group'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -241,7 +241,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'BAGEL', u'BAGEL Documentation',
-   u'Toru Shiozaki and Bess Vlasavljevich and Peter Cherry and Jae Woo Park and Ryan D. Reynolds and Hai-Anh Le and Jheng-Wei Li and Raymond Yiqun Wang and Yeonjun Jeong', 'BAGEL', 'One line description of project.',
+   u'Shiozaki Group', 'BAGEL', 'One line description of project.',
    'Miscellaneous'),
 ]
 
