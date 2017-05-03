0da4c1c964c24f5a4a4ad49ad681e7e77dd4849a
Hai-Anh Le <anh@u.northwestern.edu>
2017-05-03 00:43:04 -0500

    use computer modern for latex build

--- source/conf.py
+++ source/conf.py
@@ -205,15 +205,17 @@ htmlhelp_basename = 'BAGELdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
 latex_elements = {
   'classoptions': ',openany,oneside',
   'babel': '\\usepackage[english]{babel}',
+  'passoptionstopackages': r'\PassOptionsToPackage{svgnames}{xcolor}',
+
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
 # The font size ('10pt', '11pt' or '12pt').
 'pointsize': '10pt',
+'fontpkg' : '',
 
 # Additional stuff for the LaTeX preamble.
 'preamble':  '\definecolor{VerbatimBorderColor}{rgb}{1, 1, 1}',
